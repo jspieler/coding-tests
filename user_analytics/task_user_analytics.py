e2cc05eae026ec281a48ec2e05d8dc1f09b49c3d
Graph Dracula <07_fern.pianos@icloud.com>
2023-05-02 14:21:53 +0200

    Added statistics

--- user_analytics/task_user_analytics.py
+++ user_analytics/task_user_analytics.py
@@ -19,6 +19,28 @@ df['timestamp'] = df['timestamp'] + offset
 # Calculate the current date as the most recent date in the data
 current_date = df['timestamp'].max().date()
 
+# Calculate statistics
+users_last_24_hours = df[df['timestamp'] > dt.datetime.now().date() - pd.DateOffset(days=1)]['user'].nunique()
+teams_last_24_hours = df[df['timestamp'] > dt.datetime.now().date() - pd.DateOffset(days=1)]['team'].nunique()
+users_last_7_days = df[df['timestamp'] > dt.datetime.now().date() - pd.DateOffset(days=7)]['user'].nunique()
+teams_last_7_days = df[df['timestamp'] > dt.datetime.now().date() - pd.DateOffset(days=7)]['team'].nunique()
+users_last_30_days = df[df['timestamp'] > dt.datetime.now().date() - pd.DateOffset(days=30)]['user'].nunique()
+teams_last_30_days = df[df['timestamp'] > dt.datetime.now().date() - pd.DateOffset(days=30)]['team'].nunique()
+users_current_year = df[df['timestamp'] > dt.datetime.now().date() - pd.DateOffset(years=1)]['user'].nunique()
+teams_current_year = df[df['timestamp'] > dt.datetime.now().date() - pd.DateOffset(years=1)]['team'].nunique()
+
+statistics_output = [
+    html.P(f"Users in last 24 hours: {users_last_24_hours}", style={'font-family': 'Arial'}),
+    html.P(f"Teams in last 24 hours: {teams_last_24_hours}", style={'font-family': 'Arial'}),
+    html.P(f"Users in last 7 days: {users_last_7_days}", style={'font-family': 'Arial'}),
+    html.P(f"Teams in last 7 days: {teams_last_7_days}", style={'font-family': 'Arial'}),
+    html.P(f"Users in last 30 days: {users_last_30_days}", style={'font-family': 'Arial'}),
+    html.P(f"Teams in last 30 days: {teams_last_30_days}", style={'font-family': 'Arial'}),
+    html.P(f"Users in current year: {users_current_year}", style={'font-family': 'Arial'}),
+    html.P(f"Teams in current year: {teams_current_year}", style={'font-family': 'Arial'}),
+]
+
+
 
 if __name__ == "__main__":
     # Run this app and visit http://127.0.0.1:8050/ in your web browser.
