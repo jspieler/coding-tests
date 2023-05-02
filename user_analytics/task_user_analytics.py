971055eb5f5d9dc0c8a5aa306c369b9e415ff987
Graph Dracula <07_fern.pianos@icloud.com>
2023-05-02 14:20:44 +0200

    Import and prepare the data

--- user_analytics/task_user_analytics.py
+++ user_analytics/task_user_analytics.py
@@ -4,6 +4,21 @@ import datetime as dt
 from dash import Dash, html, dcc, callback, Input, Output
 import plotly.express as px
 
+# Read the data from data.csv
+df = pd.read_csv('data.csv')
+
+# Convert timestamp column to datetime format
+df['timestamp'] = pd.to_datetime(df['timestamp'], unit='ms')
+
+# calculate the offset between the max date and today
+offset = dt.datetime.now().date() - df['timestamp'].max().date()
+
+# Add the offset to the timestamp column
+df['timestamp'] = df['timestamp'] + offset
+
+# Calculate the current date as the most recent date in the data
+current_date = df['timestamp'].max().date()
+
 
 if __name__ == "__main__":
     # Run this app and visit http://127.0.0.1:8050/ in your web browser.
